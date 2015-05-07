5e6ed37d2265ea807caa06e29049b2bc65278373
Andrea Magatti <andrea.magatti@gmail.com>
2015-05-07 11:18:29 +0200

    django 1.8 compatibility

--- pybb/util.py
+++ pybb/util.py
@@ -148,7 +148,7 @@ def get_pybb_profile_model():
     from pybb import defaults
 
     if defaults.PYBB_PROFILE_RELATED_NAME:
-        return getattr(get_user_model(), defaults.PYBB_PROFILE_RELATED_NAME).related.model
+        return getattr(get_user_model(), defaults.PYBB_PROFILE_RELATED_NAME).related.field.model
     else:
         return get_user_model()
 
